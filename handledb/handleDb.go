68000d338192b98d6a205638923af071a14c7f9a
lucas7788 <sishsh@163.com>
2018-12-06 18:16:31 +0800

    update

--- handledb/handleDb.go
+++ handledb/handleDb.go
@@ -3,42 +3,66 @@ package handledb
 import (
 	"fmt"
 	"github.com/ontio/ontology/account"
-	cutils "github.com/ontio/ontology/cmd/utils"
 	"github.com/ontio/ontology/core/store/ledgerstore"
+	"github.com/ontio/ontology/cmd/utils"
+	"github.com/ontio/ontology/cmd/common"
+	"github.com/urfave/cli"
 	"os"
 )
 
-func AddSigToHeader(dataDir string, walletDir string) error {
-	wallet, err := account.Open(walletDir)
-	acc,err := wallet.GetAccountByAddress("AV9STaqJVr1rvibQQB3KM8mc6qYsvgiv8B",[]byte("111111"))
-	if err != nil {
-		return err
+func GetAccounts(ctx *cli.Context, walletDirs []string) ([]*account.Account,error) {
+	var accs []*account.Account
+	for i:=0;i<len(walletDirs);i++ {
+		wallet, err := account.Open(walletDirs[i])
+
+		passwd, err := common.GetPasswd(ctx)
+		if err != nil {
+			return nil, err
+		}
+		defer common.ClearPasswd(passwd)
+		acc,err := wallet.GetAccountByIndex(0, passwd)
+		if err != nil {
+			return nil, err
+		}
+		accs = append(accs, acc)
 	}
+	return accs,nil
+}
+func AddSigToHeader(dataDir,saveToDir string , accs []*account.Account) error {
+
 	blockStore, err := ledgerstore.NewBlockStore(fmt.Sprintf("%s%s%s", dataDir, string(os.PathSeparator), ledgerstore.DBDirBlock), true)
+
+	if err != nil {
+		return  fmt.Errorf("NewBlockStore error %s", err)
+	}
+	blockStore2, err := ledgerstore.NewBlockStore(fmt.Sprintf("%s%s%s", saveToDir, string(os.PathSeparator), ledgerstore.DBDirBlock), true)
+
 	if err != nil {
 		return  fmt.Errorf("NewBlockStore error %s", err)
 	}
 
 	_, blockCurrHeight, err := blockStore.GetCurrentBlock()
-	for i:=0; uint32(i) < blockCurrHeight;i++ {
+	for i:=0; uint32(i) <= blockCurrHeight;i++ {
 		blockHash,err := blockStore.GetBlockHash(uint32(i))
 		if err !=nil {
 			return  fmt.Errorf("GetBlockHash error %s", err)
 		}
 		block,err := blockStore.GetBlock(blockHash)
-		sigdata,err := cutils.Sign(blockHash.ToArray(), acc)
-		if err !=nil {
-			return  fmt.Errorf("GetBlock error %s", err)
+		for i:=0;i<len(accs);i++ {
+			sigdata,err := utils.Sign(blockHash.ToArray(), accs[i])
+			if err !=nil {
+				return  fmt.Errorf("GetBlock error %s", err)
+			}
+			block.Header.SigData = append(block.Header.SigData, sigdata)
 		}
-		block.Header.SigData = append(block.Header.SigData, sigdata)
-		blockStore.NewBatch()
-		err = blockStore.SaveBlock(block)
+		blockStore2.NewBatch()
+		err = blockStore2.SaveBlock(block)
 		if err != nil {
 			fmt.Println("SaveBlock, error:", err)
 			return err
 		}
-		blockStore.CommitTo()
+		blockStore2.CommitTo()
 	}
-	blockStore.Close()
+	blockStore2.Close()
 	return nil
 }
--- main.go
+++ main.go
@@ -3,12 +3,50 @@ package main
 import (
 	"addsigtoheader/handledb"
 	"fmt"
+	"github.com/ontio/ontology/cmd"
+	"github.com/urfave/cli"
+	"os"
+	"runtime"
 )
 
-func main(){
-	err := handledb.AddSigToHeader("/Users/sss/gopath/src/github.com/ontio/ontology/Chain/ontology",
-		"/Users/sss/gopath/src/github.com/ontio/ontology/wallet.dat")
+
+
+func setupAPP() *cli.App {
+	app := cli.NewApp()
+	app.Usage = "Ontology Tool"
+	app.Action = startHandleDb
+	app.Version = "1.0"
+	app.Copyright = "Copyright in 2018 The Ontology Authors"
+	app.Before = func(context *cli.Context) error {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		return nil
+	}
+	return app
+}
+
+func startHandleDb(ctx *cli.Context) error {
+	//原数据库目录
+	rawDbDir := "/Users/sss/gopath/src/github.com/ontio/ontology/Chain/ontology"
+	//保存到哪个目录
+	toDbDir := "/Users/sss/gopath/src/github.com/ontio/ontology/Chain/ontology2"
+	//钱包文件目录
+	walletDir := []string{
+		"/Users/sss/gopath/src/github.com/ontio/ontology/wallet.dat",
+	}
+	accs,err := handledb.GetAccounts(ctx, walletDir)
+	if err != nil {
+		return err
+	}
+	err = handledb.AddSigToHeader(rawDbDir, toDbDir, accs)
 	if err != nil {
 		fmt.Println("AddSigToHeader error:", err)
+	}
+	return nil
+}
+
+func main(){
+	if err := setupAPP().Run(os.Args); err != nil {
+		cmd.PrintErrorMsg(err.Error())
+		os.Exit(1)
 	}
 }
